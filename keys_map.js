44e3e79f24124472077360a1a14ad439faa71b87
Alex Nguyen <tiendung@users.noreply.github.com>
2021-05-27 21:53:18 +0700

    refine

--- keys_map.js
+++ keys_map.js
@@ -1,7 +1,11 @@
  /**/export let _keys_map = `
+ alh__ A-la-hán 
  blm__ ba-la-mật 
  cx___ cũng 
+ dc___ được 
  dk___ đúng không
  hk___ hiểu không
  nx___ những 
+ kk___ không 
+ rln__ rất là nhiều
  đ_đ`
