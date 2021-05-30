ef070aa169423e031f13f3c91008dbc475e0d17e
Alex Nguyen <tiendung@users.noreply.github.com>
2021-05-30 17:00:56 +0700

    refine

--- vn_keys_map.js
+++ vn_keys_map.js
@@ -5,20 +5,16 @@
  cta__ chúng ta
  cx___ cũng
  dc___ được
-dd___đ
  dk___ đúng không
  dp___ Đức Phật 
  hk___ hiểu không
  ko___ không
  kq___ kết quả
  ll___ luôn luôn
- rln__ rất là nhiều
- cta__ chúng ta
  nl___ năng lượng
  nta__ người ta
  nx___ những
  pr___ phải
- rl___ rất là
  rln__ rất là nhiều
  vc___ việc
  vx___ vẫn`
